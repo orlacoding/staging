8e9bbb7d0e650cb6ae1563e62a744ff6a6408d07
cristik <cristian.kiss@nearform.com>
2015-03-13 14:43:50 +0200

    first version of Julian's website

---
+++ src/render/scripts/scripts.js
@@ -0,0 +1,10 @@
+(function(){
+	$('.flexslider').flexslider({
+		animation: "fade",
+		animationSpeed: 500,
+		slideshowSpeed: 5000,
+		smoothHeight: true,
+		animationLoop: true,
+		touch: true
+	});
+})();
