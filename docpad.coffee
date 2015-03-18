22b41883964f7ef6be5d8795629e48d0be21ec12
cristik <cristian.kiss@nearform.com>
2015-03-18 16:57:26 +0200

    download and install page added

--- docpad.coffee
+++ docpad.coffee
@@ -3,7 +3,12 @@
 
 # Define the DocPad Configuration
 docpadConfig = {
-	# ...
+
+  # Config Paths
+  # An array of paths that we try to extract our docpad configuration from
+  configPaths: [  # default
+    'docpad.js'
+  ]
 }
 
 # Export the DocPad Configuration
---
+++ docpad.staging.js
@@ -0,0 +1,15 @@
+module.exports = {
+	port: 9778,
+
+// Template Configuration
+// Template Data
+// Use to define your own template data and helpers that will be accessible to your templates
+// Complete listing of default values can be found here: http://docpad.org/docs/template-data
+	templateData: {// example
+
+// Specify some site properties
+		site: {
+			url: 'http://senecajs.github.io/staging/'
+		}
+	}
+};
